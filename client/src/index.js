199286e151914eaf341364ed4f7ea2495f3f62af
ImDevFps <ali.tayefe1374@gmail.com>
2022-02-21 14:53:22 +0330

    uploading last files till 21feb

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store";
+import * as serviceWorker from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -16,3 +17,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+serviceWorker.register();
